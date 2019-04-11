a4324ed1c8409d937646b69c0e636adcdbadda9d
weiqing-nic <yoweiqing@gmail.com>
2019-04-11 18:28:39 +0800

    changes

--- trainer.py
+++ trainer.py
@@ -392,7 +392,7 @@ class Trainer(object):
                         for i in fake:
                             print("inside")
                             print(i.shape)
-                            vvh = torch.nn.functional.interpolate(i,size=((32,32)), mode='nearest')
+                            vvh = torch.nn.functional.interpolate(i,size=(32,32), mode='area')
                             print(type(vvh))
                             print(vvh.shape)
                             new_fake[v,:,:,:] = vvh
